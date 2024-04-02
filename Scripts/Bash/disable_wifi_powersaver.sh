261a558c1fa17ac731bab9cbe26ec7fec5e3263b
Chno <chno@odoo.com>
2024-04-02 11:23:59 -0400

    [FIX] Was missing a space after the 0 which caused a problem with the execution.

--- Scripts/Bash/disable_wifi_powersaver.sh
+++ Scripts/Bash/disable_wifi_powersaver.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# This script will be used to disble wifi powersaver permanately.
+# This script will be used to disable Wi-Fi powersaver permanently.
 
-# Check if the script is looking is running as root.
-if [ "$(id -u)" -ne "0"]; then
+# Check if the script is running as root.
+if [ "$(id -u)" -ne "0" ]; then
     echo "This script must be run as root." >&2
     exit 1
 fi
@@ -11,12 +11,11 @@ fi
 # Create the configuration file to disable Wi-Fi power saving.
 cat << EOF > /etc/NetworkManager/conf.d/wifi-powersave.conf
 [connection]
-wifi.powersaver = 2
+wifi.powersave = 2
 EOF
 
 echo "Wi-Fi power saving has been disabled."
 
 # Restart network manager.
 systemctl restart NetworkManager
-echo "Network Manager restarted." 
-
+echo "Network Manager restarted."
