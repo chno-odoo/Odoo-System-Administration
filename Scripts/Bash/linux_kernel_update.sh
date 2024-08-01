77904f0850703d1d2bd7ab43f5a45f1d01ad421d
chno <chno@odoo.com>
2024-08-01 13:43:50 -0400

    [IMP] Changed the linux_kernel_update.sh to a newer kernel update. The previous command was to update from 6.2 > 6.5. This will update from 6.8 > 6.9.1.

--- Scripts/Bash/linux_kernel_update.sh
+++ Scripts/Bash/linux_kernel_update.sh
@@ -1,7 +1,33 @@
-#!/bin/bash 
+#!/bin/bash
 
-# Thid script is used to manually apply a firmware fix that caused wifi connectivity issues.
+# This script will be used to automate the change of the Kernel from 6.8 to 6.9.1 on a fresh install for increased WiFi performance.
 
-curl -s 'https://download.odoo.com/static/misc/2024-02-29_linux_6_5_patch_firmware.patch' | sudo bash
+# Exit immediately if a command exits with a non-zero status.
+set -e
 
+# Download the kernel modules.
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.9.1/amd64/linux-headers-6.9.1-060901-generic_6.9.1-060901.202405171038_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.9.1/amd64/linux-headers-6.9.1-060901_6.9.1-060901.202405171038_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.9.1/amd64/linux-image-unsigned-6.9.1-060901-generic_6.9.1-060901.202405171038_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.9.1/amd64/linux-modules-6.9.1-060901-generic_6.9.1-060901.202405171038_amd64.deb
 
+# Install the kernel modules.
+sudo dpkg -i linux-headers-6.9.1-060901_6.9.1-060901.202405171038_all.deb
+sudo dpkg -i linux-headers-6.9.1-060901-generic_6.9.1-060901.202405171038_amd64.deb
+sudo dpkg -i linux-image-unsigned-6.9.1-060901-generic_6.9.1-060901.202405171038_amd64.deb
+sudo dpkg -i linux-modules-6.9.1-060901-generic_6.9.1-060901.202405171038_amd64.deb
+
+# Fix broken dependencies if any.
+sudo apt --fix-broken install
+
+# Update grub.
+sudo update-grub
+
+# Reboot the system with a countdown.
+echo "Rebooting in 5 seconds..."
+for i in {5..1}; do
+    echo "$i..."
+    sleep 1
+done
+echo "Rebooting now!"
+sudo reboot
