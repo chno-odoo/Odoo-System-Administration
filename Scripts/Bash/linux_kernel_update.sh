547c13b1964b2b744dc26c0d9d69720b1b3fc1df
chno <chno@odoo.com>
2024-08-01 14:32:34 -0400

    [FIX] Changed the order of installation so that linux-modules comes before unsigned.

--- Scripts/Bash/linux_kernel_update.sh
+++ Scripts/Bash/linux_kernel_update.sh
@@ -14,8 +14,8 @@ wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.9.1/amd64/linux-modules-6
 # Install the kernel modules.
 sudo dpkg -i linux-headers-6.9.1-060901_6.9.1-060901.202405171038_all.deb
 sudo dpkg -i linux-headers-6.9.1-060901-generic_6.9.1-060901.202405171038_amd64.deb
-sudo dpkg -i linux-image-unsigned-6.9.1-060901-generic_6.9.1-060901.202405171038_amd64.deb
 sudo dpkg -i linux-modules-6.9.1-060901-generic_6.9.1-060901.202405171038_amd64.deb
+sudo dpkg -i linux-image-unsigned-6.9.1-060901-generic_6.9.1-060901.202405171038_amd64.deb
 
 # Fix broken dependencies if any.
 sudo apt --fix-broken install
